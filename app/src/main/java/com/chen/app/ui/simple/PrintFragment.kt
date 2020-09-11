decf510f337b0fafc3ecd3ee3c07eaf8d9609728
chenyanqiang@mbcloud.com <chenyanqiang@mbcloud.com>
2020-09-11 18:02:06 +0800

    打印信息

--- app/src/main/java/com/chen/app/ui/simple/PrintFragment.kt
+++ app/src/main/java/com/chen/app/ui/simple/PrintFragment.kt
@@ -1,10 +1,14 @@
 package com.chen.app.ui.simple
 
+import androidx.lifecycle.lifecycleScope
 import com.alibaba.android.arouter.facade.annotation.Launch
 import com.chen.app.R
 import com.chen.baseextend.base.fragment.BaseSimpleFragment
 import com.chen.basemodule.extend.listenClick
 import kotlinx.android.synthetic.main.fragment_print.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Launch
 class PrintFragment : BaseSimpleFragment() {
@@ -15,16 +19,23 @@ class PrintFragment : BaseSimpleFragment() {
     override suspend fun onReady() {
         super.onReady()
 
-        listenClick(_print) {
-            when (it) {
-                _print -> {
-                    println(_edit.text)
-                }
-                else -> {
+        withContext(Dispatchers.Default) {
+
+            toolbar.run{
+                center("打印")
+                left(R.mipmap.ic_back) { activity?.finish() }
+            }
+
+            listenClick(_print) {
+                when (it) {
+                    _print -> {
+                        println(_edit.text)
+                    }
+                    else -> {
+                    }
                 }
             }
         }
+
     }
-
-
 }
